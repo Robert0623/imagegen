2da8d7e263fa1906d5895478202e8b5ed6804fff
hwoo <tone8943@gmail.com>
2025-03-22 18:34:52 +0900

    Initial commit

---
+++ src/test/java/com/example/imagegen/ImagegenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imagegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagegenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
